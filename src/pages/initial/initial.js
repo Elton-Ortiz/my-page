06145e680757e0b98d1214883d9601a2ebe851da
Elton Ortiz <elton.ortiz1@gmail.com>
2021-02-02 14:00:00 -0300

    homepage cards WP

--- src/pages/initial/initial.js
+++ src/pages/initial/initial.js
@@ -1,8 +1,54 @@
+import './initial.css'
+
 function Initial() {
+
+  const leftCurlyBrace  = '{';
+  const rightCurlyBrace = '}';
     return (
-      <div>
-       Initial
-      </div>
+      <main className="initial">
+        <section>
+          <div className="card">Projeto 1</div>
+          <div className="card">
+            <span>
+            <p className="class">.fazendo atualmente </p>
+            <p>{leftCurlyBrace}</p>
+            </span>
+            <span className="whitespancing">
+            <p className="attribute">lendo</p>
+            <p>: </p>
+            <p className="value">css grid layout</p>
+            <p> ;</p>
+            </span>
+            <span className="whitespancing">
+            <p className="attribute">assistindo</p>
+            <p>: </p>
+            <p className="value">vidrados</p>
+            <p> ;</p>
+            </span>
+            <span className="whitespancing">
+            <p className="attribute">jogando</p>
+            <p>: </p>
+            <p className="value">oad</p>
+            <p> ;</p>
+            </span>
+            <span className="whitespancing">
+            <p className="attribute">estudando</p>
+            <p>: </p>
+            <p className="value">svg</p>
+            <p> ;</p>
+            </span>
+            <span className="whitespancing">
+            <p className="attribute">praticando</p>
+            <p>: </p>
+            <p className="value">corrida 5km</p>
+            <p> ;</p>
+            </span>
+            <p>{rightCurlyBrace}</p>
+            </div>
+          <div className="card">Projeto 2</div>
+          <div className="card">Projeto 2</div>
+        </section>
+      </main>
     );
   }
   
